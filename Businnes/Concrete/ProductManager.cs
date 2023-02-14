b02fb1ee56293b491a537c6d68bca57728f1aad5
serhovski <sero41fb@outlook.com>
2023-02-14 13:55:36 +0300

    14 February Commit

--- Businnes/Concrete/ProductManager.cs
+++ Businnes/Concrete/ProductManager.cs
@@ -33,7 +33,7 @@ namespace Businnes.Concrete
             _categoryService = categoryService;
         }
 
-        //[SecuredOperation("product.add,admin")]
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
